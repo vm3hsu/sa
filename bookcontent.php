ec07aa1dfb03e34e4d63867174e058d29ab39f3b
Kai <kkwang0921@gmail.com>
2023-04-27 21:58:06 +0800

    bookcontent

---
+++ bookcontent.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Ogani Template">
+    <meta name="keywords" content="Ogani, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ogani | Template</title>
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;900&display=swap" rel="stylesheet">
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<style type="text/css">
+    #DIV_A {
+        box-shadow: 3px 3px 9px yellow;
+    }
+
+    #login {
+        position: absolute;
+        width: auto;
+        height: auto;
+        top: auto;
+        right: 0;
+        bottom: 10%;
+        left: auto;
+    }
+
+    #menu {
+        position: relative;
+        width: auto;
+        height: auto;
+        top: auto;
+        right: auto;
+        bottom: auto;
+        left: 5%;
+    }
+</style>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+    <!-- Humberger Begin -->
+    <div class="humberger__menu__overlay"></div>
+    <div class="humberger__menu__wrapper">
+        <div class="humberger__menu__logo">
+            <a href="#"><img src="img/logo.png" alt=""></a>
+        </div>
+        <nav class="humberger__menu__nav mobile-menu">
+            <ul>
+                <li><a href="index.php">首頁</a></li>
+                <li class="active"><a href="shop.php">瀏覽二手書</a></li>
+                <li><a href="request.php">索書專區</a></li>
+                <li><a href="common.php">常見問題</a></li>
+                <li><a href="QA.php">提問專區</a></li>
+                <li><a href="member.php">會員專區</a></li>
+            </ul>
+        </nav>
+        <div id="mobile-menu-wrap"></div>
+    </div>
+    <!-- Humberger End -->
+    <!-- Header Section Begin -->
+    <header class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5">
+                    <div class="header__logo">
+                        <a href="index.php"><img src="img/logo1.png"></a>
+                    </div>
+                </div>
+                <div class="col-lg-7">
+                    <div class="header__cart">
+                        <table id="login">
+                            <?php
+                                session_start();
+                                if($_SESSION['name']=="")
+                                {
+                                    echo"<tr><td>[<a href='SAlogin.php'>登入</a>]</td><td>[<a href='register.php'>註冊</a>]</td></tr>";
+                                }
+                                else
+                                {
+                                    echo"<tr><td>".$_SESSION['name']."，你好</td><td>[<a href='logout.php'>登出</a>]</td></tr>";
+                                }
+                            ?>
+                            
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="humberger__open">
+                <i class="fa fa-bars"></i>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <form action="#" style="border: 2px solid #4569ff;border-radius: 10px;overflow: hidden; position: relative; width: auto; height: auto; top: 30%; right: auto; bottom: auto; left: auto;">
+                        <input type="text" placeholder="搜尋書名" style="width: 80%">
+                        <button type="submit" style="border: none;background-color: #ffffff;color: #fff;width: 15%"><img src="img/search.png" style="width:85%;height:85%"></button>
+                    </form>
+                </div>
+                <div class="col-lg-9">
+                    <nav class="header__menu" id="menu">
+                        <ul>
+                            <li><a href="index.php">首頁</a></li>
+                            <li class="active"><a href="shop.php">瀏覽二手書</a></li>
+                            <li><a href="request.php">索書專區</a></li>
+                            <li><a href="common.php">常見問題</a></li>
+                            <li><a href="QA.php">提問專區</a></li>
+                            <li><a href="member.php">會員專區</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header Section End -->
+
+
+    <!-- Blog Section Begin -->
+    <section class="hero">
+        <div class="container">
+            <div class="row">
+            <?php include "sidebar.php";?>
+                <div class="col-lg-8 col-md-8">
+                    <div class="row">
+                        <div class="col-sm-12 col-xs-12 site_map" style="margin-bottom: 15px;">
+				            <span>
+					        <a class="linkStyle01" href="index.php">首頁</a>
+				            </span>
+				            <span class="span01">&gt;</span><span class="span02"><a class="linkStyle01" href="shop.php">買二手書</a></span>
+				            <span class="span01">&gt;</span><span class="span02"><a class="linkStyle01" href="index.php">管理學院</a></span>
+                            <hr size="2px" align="top" width="100%">
+			
+                        </div>
+                        <div class="col-lg-5 col-md-5 col-sm-5">
+                            <div class="blog__item">
+                                <div class="blog__item__pic">
+                                    <img src="img/blog/blog-2.jpg" alt="">
+                                </div>
+                                <div class="blog__item__text" align="right">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="authorBrand">
+                            <h1 style="line-height: 24px;letter-spacing: 0.5px;font-size:20px;font-weight:bold; letter-spacing:1px;margin: 0;">經濟學<span style="color:#755e5f; font-weight: normal;font-size:20px;"></span></h1>
+		                    <hr size="2px" align="top" width="100%">
+                            <p style="margin:10px 0 0 0;"><span>賣家：范茵茜</span></p>
+		                    <p style="margin:10px 0 0 0;"><span>LINE ID：<span>09757xxxxx</span></p>
+		                    <p style="margin:10px 0 0 0;"><span>售價：<span>450元</span></p>
+		                    <p style="margin:10px 0 0 0;"><span>書況補充說明：<span>自然泛黃、髒汙</span></p>
+
+                            <input type="submit" value="加入購物車">
+                            <input type="submit" value="確認購買">
+	                    </div>
+                        
+
+                        <div class="col-lg-7 col-md-7 col-sm-7">
+                            <div class="row">
+                                <div class="blog__item__pic">
+                                    <img src="img/blog/blog-2.jpg" alt="">
+                                </div>
+                                <div class="blog__item__text" align="right">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Blog Section End -->
+    <!-- Footer Section Begin -->
+    <footer class="footer spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="footer__about">
+                        <div class="footer__about__logo">
+                            <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                        </div>
+                        <ul>
+                            <li>Address: 60-49 Road 11378 New York</li>
+                            <li>Phone: +65 11.188.888</li>
+                            <li>Email: hello@colorlib.com</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 offset-lg-1">
+                    <div class="footer__widget">
+                        <h6>Useful Links</h6>
+                        <ul>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">About Our Shop</a></li>
+                            <li><a href="#">Secure Shopping</a></li>
+                            <li><a href="#">Delivery infomation</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Our Sitemap</a></li>
+                        </ul>
+                        <ul>
+                            <li><a href="#">Who We Are</a></li>
+                            <li><a href="#">Our Services</a></li>
+                            <li><a href="#">Projects</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">Innovation</a></li>
+                            <li><a href="#">Testimonials</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12">
+                    <div class="footer__widget">
+                        <h6>Join Our Newsletter Now</h6>
+                        <p>Get E-mail updates about our latest shop and special offers.</p>
+                        <form action="#">
+                            <input type="text" placeholder="Enter your mail">
+                            <button type="submit" class="site-btn">Subscribe</button>
+                        </form>
+                        <div class="footer__widget__social">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-pinterest"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer__copyright">
+                        <div class="footer__copyright__text">
+                            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                Copyright &copy;<script>
+                                    document.write(new Date().getFullYear());
+                                </script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                        </div>
+                        <div class="footer__copyright__payment"><img src="img/payment-item.png" alt=""></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Footer Section End -->
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/mixitup.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+sa/blog.php at test · vm3hsu/sa
