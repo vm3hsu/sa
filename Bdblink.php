ef76a00707c33ff9cc6d20a208a66aec317dcd0b
Hsu <oscar20021010@gmail.com>
2023-05-08 17:01:19 +0800

    上傳照片

--- Bdblink.php
+++ Bdblink.php
@@ -20,6 +20,16 @@
     $price = $_POST['price'];
     $selled = 0;
     $seller = $_SESSION['account'];
+    $image_new_name = "1";
+    $_FILES["file"]['tmp_name'] = "1";
+    $image_upload_path = "bookpicture/".$_FILES["file"]['tmp_name'] ;
+
+        echo"檔案名稱".$_FILES["file"]['name']."<br/>";
+        echo"檔案類型".$_FILES["file"]['type']."<br/>";
+        echo"檔案大小".($_FILES["file"]['size']/1024)."KB<br/>";
+        echo"暫存名稱".$_FILES["file"]['tmp_name']."<br/>";
+    
+        $is_upload = move_uploaded_file($_FILES["file"]["name"],$image_upload_path);
 
     
     $link = mysqli_connect('localhost', 'root', '12345678', 'sa');
