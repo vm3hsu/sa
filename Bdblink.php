d277994465521e08e6745bd80770378cc66791b7
Hsu <oscar20021010@gmail.com>
2023-05-16 13:27:59 +0800

    照片

--- Bdblink.php
+++ Bdblink.php
@@ -21,27 +21,33 @@
     $selled = 0;
     $seller = $_SESSION['account'];
     $image_new_name = "1";
-    $image_upload_path = "/bookpicture/";
+    $image_upload_path = "bookpicture/";
 
         echo"檔案名稱".$_FILES["file"]['name']."<br/>";
         echo"檔案類型".$_FILES["file"]['type']."<br/>";
         echo"檔案大小".($_FILES["file"]['size']/1024)."KB<br/>";
         echo"暫存名稱".$_FILES["file"]['tmp_name']."<br/>";
         echo $_FILES['file']['error'];
-
-        $is_upload = move_uploaded_file($_FILES["file"]["tmp_name"],"bookpicture/".$_FILES["file"]['name']);
-        if($is_upload){
-            echo"成功".$is_upload;
-        }
-        else{
-            echo"失敗".$is_upload;
-        }
+        echo basename($_FILES["file"]['name'],'jpg');
     
     $link = mysqli_connect('localhost', 'root', '12345678', 'sa');
     if ($dbaction == "insert") {
         $sql = "insert into book (BNumber, BName, BCondition, category, college, price, selled, seller) values (NULL, '$BName', '$BCondition', '$category', '$college', '$price', '$selled', '$seller')";
         if (mysqli_query($link, $sql)) {
             //echo "新增成功";
+            $sql = "SELECT * FROM book ORDER BY BNumber DESC LIMIT 1";
+            $result = mysqli_query($link, $sql);
+            while ($row = mysqli_fetch_assoc($result)) {
+                $BNumber = $row['BNumber'];
+            }
+            echo $BNumber;
+            $path = "bookpicture/".$BNumber;
+            if (!is_dir($path)) {
+                mkdir($path, 0777, true);
+            }
+            $is_upload = move_uploaded_file($_FILES["file"]["tmp_name"],"bookpicture/".$BNumber."/".$_FILES["file"]['name']);
+            rename("bookpicture/".$BNumber.$_FILES["file"]['name'],"bookpicture/".$BNumber.'1.png');
+            echo rename("bookpicture/".$BNumber.$_FILES["file"]['name'],"bookpicture/".$BNumber.'1.png');
             header("location:message.php?message=新增成功");
         } else {
             //echo "新增失敗";
