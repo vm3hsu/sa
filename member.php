8412d02dd2bc3a2589c0c152b3af5f73785d9496
jorden <jorden920526@gmail.com>
2023-05-16 17:39:22 +0800

    購物車圖標

--- member.php
+++ member.php
@@ -89,6 +89,8 @@
                                 else
                                 {
                                     echo"<tr><td>".$_SESSION['name']."，你好</td><td>[<a href='logout.php'>登出</a>]</td></tr>";
+                                    echo"<tr><td colspan=2>
+                                    <div><a href='member.php'><svg xmlns='http://www.w3.org/2000/svg' width='25' height='30' fill='currentColor' class='bi bi-cart4' viewBox='0 0 16 16'><path d='M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z'/></svg><font style='font-size: x-large;vertical-align: top;'>購物車</font></a></div></td></tr>";
                                 }
                             ?>
                             
@@ -187,7 +189,7 @@
                                     </tbody>
                                 </table>
 
-
+                                <hr size="2px" align="top" width="100%">
                                 <table class="table table-bordered border-warning table-hover">
                                     <thead style="background-color:#7fad39">
                                         <tr>
@@ -221,10 +223,11 @@
                                 </table>
                             </div>
                             <div id="mid">
+                            <hr size="2px" align="top" width="100%">
                                 <h3>我是賣家<br>
                                     <hr size="2px" align="top" width="100%">
                                 </h3>
-                                <p>上架欲賣出書籍請點此填寫表單</p>
+                                
 
                                 <table class="table table-bordered border-warning table-hover">
                                     <thead style="background-color:#7fad39">
