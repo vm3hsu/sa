5674f241980fc6e80d73a8063d128d61c61ca834
cherry <cherryt713jk@gmail.com>
2023-06-07 18:12:31 +0800

    QA1.php

--- QA1.php
+++ QA1.php
@@ -176,11 +176,18 @@
                             <span style="font-family:DFKai-sb;">
                                 <div id="top">
                                 <div style="background-color:#FAFAFA">
-                                    
+                                        <h3><font size="5">&emsp;回饋專區___請告訴我們的工程師你對於此網站的改善&emsp;<br>&emsp;與建議</font>
+                                            <hr size="2px" align="top" width="100%">
+                                        </h3>
+                                        <form>
+                                            &emsp;歡迎你們提供對於這個平台更好的改善方式：<br>&emsp;<textarea name="comments" rows="8" cols="60"></textarea>
+                                            <input type="submit" value="送出資料"><br>
+                                            <br>
+                                        </form>
                                         <h3><font size="5">&emsp;負評專區___待您送出後我們會將此評價顯示在對象的個人檔&emsp;<br>&emsp;案中</font>
                                             <hr size="2px" align="top" width="100%">
                                         </h3>
-                                        
+                                        <form>
                                             &emsp;投訴對象名稱：<input type="text" name="target">                                        
                                             <br><br>
                                             <td>
@@ -198,7 +205,16 @@
                                             <input type="submit" value="送出資料"><br>
                                             <br>
                                         </form>
-                                    
+                                        <h3><font size="5">&emsp;好評專區___待您送出後我們會將此評價顯示在對象的個人檔&emsp;<br>&emsp;案中</font>
+                                            <hr size="2px" align="top" width="100%">
+                                        </h3>
+                                        <form>
+                                            &emsp;好評對象名稱：<input type="text" name="target">                                        
+                                            <br><br>
+                                            &emsp;給予好評原因(請詳細說明):<br>&emsp;<textarea name="content" rows="5" cols="60"></textarea>
+                                            <input type="submit" value="送出資料"><br>
+                                            <br>
+                                        </form>
                                 </div>
                                 </div>
                             </span>
