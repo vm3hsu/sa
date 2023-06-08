f1b66dfe4caaf9eb4c0286308036fb88e9734ce1
Hsu <oscar20021010@gmail.com>
2023-06-08 16:48:09 +0800

    QA

--- QAdblink.php
+++ QAdblink.php
@@ -23,9 +23,9 @@
     $sql = "select * from user u where u.name = '$target' ";
     $result = mysqli_query($link, $sql);
     $row = mysqli_fetch_assoc($result);
-    $buyer = $row['account'];
+    $buyer = $row['name'];
 
-    $sql = "INSERT INTO `rate` (`rateNumber`, `writer`, `target`, `content`) VALUES (NULL, ".$_SESSION['account'].", $buyer, '$content')";
+    $sql = "INSERT INTO `rate` (`rateNumber`, `writer`, `target`, `content`) VALUES (NULL, ".$_SESSION['name'].", $buyer, '$content')";
     if(mysqli_query($link, $sql)){
 
         header("location:message.php?message=新增成功");
